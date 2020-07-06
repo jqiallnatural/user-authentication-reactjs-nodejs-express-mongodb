75634072f1f15d09148a83eba1d43eeb59c1b4cb
Jiaqi Zhao <jiaqi.zhao.tr@gmail.com>
2020-07-06 17:59:27 -0400

    ✨ Implement JWT

--- auth-backend/routes/userRouter.js
+++ auth-backend/routes/userRouter.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 router.post("/register", async (req, res) => {
@@ -9,9 +10,7 @@ router.post("/register", async (req, res) => {
     // Validation
 
     if (!email || !password || !passwordCheck)
-      return res
-        .status(400)
-        .json({ msg: "Not all fields have been entered." });
+      return res.status(400).json({ msg: "Not all fields have been entered." });
     if (password.length < 6)
       return res
         .status(400)
@@ -31,20 +30,49 @@ router.post("/register", async (req, res) => {
 
     // Check for null & undefined
 
-    if (!displayName) displayName = email
+    if (!displayName) displayName = email;
 
-    const salt = await bcrypt.genSalt()
-    const passwordHash = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
     const newUser = new User({
       email,
       password: passwordHash,
-      displayName
-    })
-    const savedUser = await newUser.save()
-    res.json(savedUser)
-
+      displayName,
+    });
+    const savedUser = await newUser.save();
+    res.json(savedUser);
   } catch (err) {
     res.status(500).json({ error: err.message }); // Internal server error and give it to front end
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validate
+    if (!email || !password)
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+
+    const user = await User.findOne({ email: email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ msg: "No account with this email has been registered." });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRETS);
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        displayName: user.displayName,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
