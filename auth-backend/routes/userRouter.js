e59d0c30ebf0671163332c85c92fd4e19114d35c
Jiaqi Zhao <jiaqi.zhao.tr@gmail.com>
2020-07-07 01:04:55 -0400

    ✨ Implement delete

--- auth-backend/routes/userRouter.js
+++ auth-backend/routes/userRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 const User = require("../models/userModel");
 
 router.post("/register", async (req, res) => {
@@ -75,5 +76,32 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+router.delete("/delete", auth, async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.user);
+    res.json(deletedUser)
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Use this end point to verify token
+router.post("/tokenIsValid", async ( req, res ) => {
+  try {
+    const token = req.header("x-auth-token")
+    if (!token) return res.json(false)
+
+    const verified = jwt.verify(token, process.env.JWT_SECRETS)
+    if (!verified) return res.json(false)
+
+    const user = await User.findById(verified.id)
+    if(!user) return res.json(false)
+
+    return res.json(true)
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+})
 
 module.exports = router;
